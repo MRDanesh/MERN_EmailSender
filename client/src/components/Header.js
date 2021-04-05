6c8a8d6f0640fcda6ebaaade946443d27c42ce94
MRDanesh <Mohammadrezad126@gmail.com>
2021-04-05 16:14:42 -0400

    add Stripe payment to project

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends React.Component{
 
     renderContent = () => {
@@ -11,12 +13,14 @@ class Header extends React.Component{
             case false: 
                 return <li><a href='/auth/google'>Login with Google</a></li>
             default:
-                return <li><a href='/api/logout'>Logout</a></li>
+                return [
+                    <li key='1'><Payments/></li>,
+                    <li key='2'><a href='/api/logout'>Logout</a></li>
+                ]
         }
     }
 
     render() {
-        console.log(this.props);
         return (
             <nav>
                 <div className='nav-wrapper'>
